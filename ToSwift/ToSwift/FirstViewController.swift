d55fd117c50301e0afd0872fe1f0861bb9e87d85
Codevski <1435321+codevski@users.noreply.github.com>
2018-09-25 17:24:00 +1000

    init

---
+++ ToSwift/ToSwift/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToSwift
+//
+//  Created by Sash Petrovski on 25/9/18.
+//  Copyright © 2018 Gitup. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
