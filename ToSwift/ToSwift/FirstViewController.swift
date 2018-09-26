8ce22c19896ef70067aab94ae27d8898af2417a6
Codevski <1435321+codevski@users.noreply.github.com>
2018-09-26 12:04:02 +1000

    Basic Functionality

--- ToSwift/ToSwift/FirstViewController.swift
+++ ToSwift/ToSwift/FirstViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var table: UITableView!
+    
+    var totalItems = UserDefaults.standard.array(forKey: "items")
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return totalItems?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = (totalItems![indexPath.row] as! String)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == UITableViewCell.EditingStyle.delete) {
+            print("Need To Delete \(totalItems![indexPath.row])")
+            totalItems!.remove(at: indexPath.row)
+            UserDefaults.standard.set(totalItems, forKey: "items")
+            viewDidAppear(true)
+        }
+    }
+    
+    func updateItems() {
+        totalItems = UserDefaults.standard.array(forKey: "items")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("UPDATING 1")
+        updateItems()
+        self.table.reloadData()
+        
     }
 
 
--- ToSwift/ToSwift/SecondViewController.swift
+++ ToSwift/ToSwift/SecondViewController.swift
@@ -8,11 +8,67 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var itemInput: UITextField!
+    
+    var items = [String]()
+    
+    @IBAction func add(_ sender: Any) {
+        print("Added Item")
+        
+        if let item = itemInput.text {
+            print("append")
+            
+            items.append(item)
+            
+            UserDefaults.standard.set(items, forKey: "items")
+            
+            itemInput.text = ""
+        }
+        
+        
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        UserDefaults.standard.removeObject(forKey: "items")
+        if UserDefaults.standard.array(forKey: "items") != nil {
+            items = (UserDefaults.standard.array(forKey: "items") as? Array)!
+        }
+        
+    }
+    
+    // Hide keyboard when text field is not in focus
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.view.endEditing(true)
+         print(items)
+        
+        if let test = UserDefaults.standard.array(forKey: "items") {
+            print(test)
+        }
+        
+    }
+    
+    // Hide keyboard when hit return on virtual keyboard
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("UPDATING 2")
+        print(UserDefaults.standard.array(forKey: "items"))
+        
+        if UserDefaults.standard.array(forKey: "items") != nil {
+            items = (UserDefaults.standard.array(forKey: "items") as? Array)!
+        }
     }
 
 
